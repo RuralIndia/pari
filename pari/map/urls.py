a2e8e164c2411fbf3feafccb3970e24e03b8dba1
manojlds <manojlds@gmail.com>
2013-02-22 10:46:42 +0530

    Moving map related static files to within the app

--- pari/map/urls.py
+++ pari/map/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = patterns('pari.map.views',
     url(r'^$', 'index'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
