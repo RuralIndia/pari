0f60a56cd4b8e4ae2157e1a1d01f1bbc3644ded7
manojlds <manojlds@gmail.com>
2013-04-26 11:39:35 +0530

    Refactoring to do pagination and filtering

--- pari/article/static/article/js/article_filter.js
+++ pari/article/static/article/js/article_filter.js
@@ -1,22 +1,44 @@
 var ArticleFilter = {
     init: function() {
-        $('.type-filter').click(function(){
-        var filterEndpoint = $('#article-list').data('filter-endpoint');
-        var args = {};
-        if(!($(this).hasClass('active'))){
-            args['filter'] = $(this).data('filter');
-        }
+        $('.type-filter').on('click', $.proxy(function(event){
+            var filterEndpoint = $('#article-list').data('filter-endpoint');
+            var filterElement = $(event.target).parent('.type-filter');
+            if(filterElement.hasClass('active')){
+                $('#article-list').removeData('filter-args-filter');
+            } else {
+                $('#article-list').data('filter-args-filter', filterElement.data('filter'));
+            }
+
+            this.collectArgsAndSumbit();
+        }, this));
+
+        $('[data-page]').on('click', $.proxy(function(event){
+            event.preventDefault();
+            var paginationElement = $(event.target).closest('li');
+
+            $('#article-list').data('filter-args-page', paginationElement.data('page'));
+
+            this.collectArgsAndSumbit();
+        }, this));
+    },
+
+    collectArgsAndSumbit: function(){
         var filterArgsPrefix = "filterArgs";
+        args = {};
+
         $.each($('#article-list').data(), function(key,value){
             if(key.substring(0, filterArgsPrefix.length) === filterArgsPrefix){
                 var arg = key.replace(filterArgsPrefix,'').toLowerCase();
                 args[arg] = value;
             }
-
         });
 
+        var filterEndpoint = $('#article-list').data('filter-endpoint');
+        this.submit(filterEndpoint, args);
+    },
+
+    submit: function(filterEndpoint, args){
         Dajaxice.pari.article[filterEndpoint](Dajax.process, args);
-    });
     }
 }
 
