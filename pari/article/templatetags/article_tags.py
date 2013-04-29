a1f5eff49b8eb146d11c87d9e0e85e3ce06d0e9c
manojlds <manojlds@gmail.com>
2013-04-29 13:00:15 +0530

    Adding filter argument to template tag

--- pari/article/templatetags/article_tags.py
+++ pari/article/templatetags/article_tags.py
@@ -17,5 +17,5 @@ def featured_content_for(article):
 
 
 @register.inclusion_tag("article/includes/article_list.html")
-def article_list(articles, title, types):
-    return {'articles': articles, 'title': title, 'types': types}
+def article_list(articles, title, types, filter):
+    return {'articles': articles, 'title': title, 'types': types, 'filter': filter}
