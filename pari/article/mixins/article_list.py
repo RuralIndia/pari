54d9d63c582f09b6331799eb65a589c78ac13912
manojlds <manojlds@gmail.com>
2013-04-29 11:02:03 +0530

    Adding page and filter to the context

--- pari/article/mixins/article_list.py
+++ pari/article/mixins/article_list.py
@@ -4,13 +4,16 @@ from pari.article.ajax import get_article_list
 class ArticleListMixin(object):
     article_list_context_name = "articles"
 
-    def get_article_list(self):
+    def get_article_list(self, page, filter):
         article_queryset = self.get_article_list_queryset()
-        page = self.request.GET.get('page')
-        filter = self.request.GET.get('filter')
+        
         return get_article_list(article_queryset, page, filter)
 
     def get_context_data(self, **kwargs):
         context = super(ArticleListMixin, self).get_context_data(**kwargs)
-        context[self.article_list_context_name] = self.get_article_list()
+        page = self.request.GET.get('page')
+        filter = self.request.GET.get('filter')
+        context[self.article_list_context_name] = self.get_article_list(page, filter)
+        context['page'] = page
+        context['filter'] = filter
         return context
