fcbcd1f8744483bfded9d22c2d3eb47a0f1f5639
manojlds <manojlds@gmail.com>
2013-04-29 16:14:13 +0530

    Adding audio in the article list

--- pari/article/templatetags/article_tags.py
+++ pari/article/templatetags/article_tags.py
@@ -3,8 +3,8 @@ from mezzanine import template
 register = template.Library()
 
 
-@register.inclusion_tag("article/includes/capsule_video.html")
-def capsule_video_for(article, width, height):
+@register.inclusion_tag("article/includes/list_media.html")
+def list_media_for(article, width, height):
     return {'article': article,
             'width': width,
             'height': height,
