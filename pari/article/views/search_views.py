9b3b300b235d7498800dd16858c275e30b253f30
Suganthi T <suganthikrishnavathi@gmail.com>
2013-04-30 21:38:50 +0530

    Directly hitting url will return filtered search result

--- pari/article/views/search_views.py
+++ pari/article/views/search_views.py
@@ -4,8 +4,9 @@ from mezzanine.utils.views import render
 
 def search_detail(request):
     query = request.GET.get("query", "")
+    filter = request.GET.get("filter", "")
     results = Displayable.objects.search(query)
     result_types = [subclass.__name__ for subclass in Displayable.__subclasses__() if "pari" in subclass.__module__]
     templates = [u"article/search_detail.html"]
-    c = {"query": query, "results": results, "result_types": result_types}
+    c = {"query": query, "results": results, "result_types": result_types, "filter": filter}
     return render(request, templates, c)
