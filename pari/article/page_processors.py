40e4baadb650813ca01d4742b3eed0aefd6b8a09
manojlds <manojlds@gmail.com>
2013-04-30 15:36:44 +0530

    Fixing the caption height in categories page

--- pari/article/page_processors.py
+++ pari/article/page_processors.py
@@ -6,7 +6,7 @@ from .models import Article, Category
 def homepage_context(request, page):
     article_list = Article.articles.filter(featured_image__isnull=False)[:5]
     categories = Category.objects.all()
-    recent_articles = Article.articles.order_by('-publish_date')[:4]
+    recent_articles = Article.articles.order_by('-publish_date')[:6]
     return {
         "article_list": article_list,
         "recent_articles": recent_articles,
