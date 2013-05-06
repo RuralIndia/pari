17c3713b1a0628108cbce5d634b48174a9d60d55
manojlds <manojlds@gmail.com>
2013-05-06 15:04:19 +0530

    Locating the user on the map page

---
+++ pari/map/static/map/js/map_mobile.js
@@ -0,0 +1,16 @@
+$(function () {
+    var options = {center : new L.LatLng(21.7679,78.8718), zoom : 3 };
+    
+    var mapUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
+        osm = new L.TileLayer(mapUrl, {maxZoom: 3});
+    
+    var mapLayer = new L.TileLayer(mapUrl);
+    
+    var map = new L.Map('location-map', options).addLayer(mapLayer);
+    map.locate({setView : true, maxZoom: 7});
+
+    map.on('locationfound', function(e) {
+        L.marker(e.latlng).addTo(map);
+    });
+
+});
