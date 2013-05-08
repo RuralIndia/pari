9f7f94af4c8bf93783a16f757c09575911a2bf05
Suganthi T <suganthikrishnavathi@gmail.com>
2013-05-08 19:16:36 +0530

    Added back button to photos page

--- pari/album/static/album/js/stapel.js
+++ pari/album/static/album/js/stapel.js
@@ -1,3 +1,21 @@
 $(function () {
-    $( '#tp-grid' ).stapel()
+    var $grid = $( '#tp-grid' ),
+        $name = $( '#name' ),
+        $close = $( '#close' ),
+        $loader = $( '<div class="loader"><i></i><i></i><i></i><i></i><i></i><i></i><span>Loading...</span></div>' ).insertBefore( $grid ),
+        stapel = $grid.stapel( {
+            onLoad : function() {
+                $loader.remove();
+            },
+            onBeforeOpen : function( pileName ) {
+                $name.html( pileName );
+                $close.show();
+            }
+        } );
+
+    $close.on( 'click', function() {
+        $close.hide();
+        $name.empty();
+        stapel.closePile();
+    } );
 });
