e13f8b1e3533c1fdc71fac3cc1b3d488a3421464
manojlds <manojlds@gmail.com>
2013-05-16 12:24:42 +0530

    Refactoring map js

--- pari/map/static/map/js/map.js
+++ pari/map/static/map/js/map.js
@@ -1,30 +1,49 @@
-$(function(){
-    var source = $("#article-template").html();
-    var template = Handlebars.compile(source);
-    $.get('/article/api/locations/?format=json', function(data) {
-        $('#map').vectorMap({
-          map: 'in_mill_en',
-          regionStyle: {
-            initial: {
-              fill: 'grey'
-            }
-          },
-          backgroundColor: '#ffffff',
-          markers: data,
-          markerStyle: {
-            initial: {
-                fill: '#F8E23B',
-                stroke: '#383f47'
-            }
-          },
-          onMarkerClick: function(e, index){
-            var id = data[index].id;
-            $.get('/article/api/locations/' + id + '/article/?format=json', function(locationData) {
-              var templateHtml= template(locationData);
-              $("#side").html(templateHtml);
-            });
+var mapInterface = {
+    init: function() {
+        this.template = Handlebars.compile(this.source);
+        this.populateLocations();
+    },
 
-          }
-        });
-    });
+    populateLocations: function() {
+        $.get('/article/api/locations/?format=json', $.proxy(function(data) {
+            this.data = data;
+            $('#map').vectorMap({
+                map: 'in_mill_en',
+                regionStyle: {
+                    initial: {
+                        fill: 'grey'
+                    }
+                },
+                backgroundColor: '#ffffff',
+                markers: data,
+                markerStyle: {
+                    initial: {
+                        fill: '#F8E23B',
+                        stroke: '#383f47'
+                    }
+                },
+                onMarkerClick: $.proxy(function(e, index){
+                    this.onMarkerClick(e, index);
+                }, this)
+            });
+        }, this));
+    },
+
+    onMarkerClick: function (e, index) {
+        var id = this.data[index].id;
+        $.get('/article/api/locations/' + id + '/article/?format=json', $.proxy(function(locationData) {
+            var templateHtml= this.template(locationData);
+            $("#side").html(templateHtml);
+        }, this));
+
+    },
+
+    source: $("#article-template").html(),
+
+    data: null
+};
+
+
+$(function(){
+    mapInterface.init()
 });
