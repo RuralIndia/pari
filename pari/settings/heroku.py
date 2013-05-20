8da707ecfdd17c4300896245c2e05c8fb5f25c57
manojlds <manojlds@gmail.com>
2013-05-20 14:10:51 +0530

    Using S3 only for serving in heroku

--- pari/settings/heroku.py
+++ pari/settings/heroku.py
@@ -12,7 +12,7 @@ COMPRESS_ENABLED = True
 
 INSTALLED_APPS += (
     "django_extensions",
-    "storages",
+    # "storages",
 )
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
@@ -20,16 +20,16 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_QUERYSTRING_AUTH = False
 
-DEFAULT_FILE_STORAGE = 'pari.article.storage.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'pari.article.storage.StaticRootS3BotoStorage'
-COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
+# DEFAULT_FILE_STORAGE = 'pari.article.storage.MediaRootS3BotoStorage'
+# STATICFILES_STORAGE = 'pari.article.storage.StaticRootS3BotoStorage'
+# COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
 
 S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = S3_URL
-COMPRESS_URL = STATIC_URL
-MEDIA_URL = STATIC_URL + "media/"
-MEDIA_ROOT = ''
+# STATIC_URL = S3_URL
+# COMPRESS_URL = STATIC_URL
+MEDIA_URL = S3_URL + "media/"
+# MEDIA_ROOT = ''
 
-STATICFILES_FINDERS += (
-    "django.contrib.staticfiles.finders.DefaultStorageFinder",
-)
+# STATICFILES_FINDERS += (
+    # "django.contrib.staticfiles.finders.DefaultStorageFinder",
+# )
