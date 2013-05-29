c59af665857bf0d855da6b96a08f6c989d3f3c2f
Suganthi T <suganthikrishnavathi@gmail.com>
2013-05-29 14:58:45 +0530

    Album cannot be added without an image/zip file

--- pari/album/forms.py
+++ pari/album/forms.py
@@ -4,5 +4,5 @@ from django.core.exceptions import ValidationError
 
 class AlbumImageInlineFormset(forms.models.BaseInlineFormSet):
     def clean(self):
-        if not self.cleaned_data[0]:
+        if not self.cleaned_data[0] and not self.instance.zip_import:
             raise ValidationError(u'Upload atleast one image')
