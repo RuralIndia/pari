9a7628c2fa90bd701f7c42ab5890ca29740ed469
manojlds <manojlds@gmail.com>
2013-05-29 17:18:04 +0530

    Ability to add captions to images

--- pari/static/js/tinymce_setup.js
+++ pari/static/js/tinymce_setup.js
@@ -1,6 +1,7 @@
 if (typeof tinyMCE != 'undefined') {
     tinymce.init({
         selector: ".mceEditor",
+        height : 400,
         file_browser_callback: function (field_name, url, type, win) {
             var frame = tinyMCE.activeEditor.windowManager.open({
                 file: window.__filebrowser_url + '?pop=2&type=' + type,
@@ -27,19 +28,30 @@ if (typeof tinyMCE != 'undefined') {
             "advlist autolink lists link image charmap print preview hr anchor pagebreak",
             "searchreplace wordcount visualblocks visualchars code fullscreen",
             "insertdatetime media nonbreaking save table contextmenu directionality",
-            "template paste autoresize"
+            "template paste caption"
         ],
-        autoresize_min_height: "400",
-        autoresize_max_height: "800",
         content_css: "/static/css/tinymce.css",
 
-        toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+        toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image caption droptext",
         toolbar2: "fullscreen print preview media | forecolor backcolor",
         image_advtab: true,
         templates: [
             {title: 'Test template 1', content: 'Test 1'},
             {title: 'Test template 2', content: 'Test 2'}
         ],
+        formats : {
+            droptext: {inline: 'span', classes: "droptext"}
+        },
+
+        setup: function(ed) {
+            ed.addButton('droptext', {
+                text: 'Drop Text',
+                icon: false,
+                onclick: function() {
+                    ed.formatter.apply('droptext');
+                }
+            });
+        }
     });
 
 }
