813be92c0a3b8582800fb8213b4da7180f5754d1
Jayakumar Chinnappan <chinnappan.jayakumar@gmail.com>
2013-06-01 18:29:17 +0530

    Pep8 compliance. No logical changes.

--- test/base/base_test.py
+++ test/base/base_test.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 
 
 class Test(unittest.TestCase):
-    
+
     def setup_method(self, method):
-        self.driver = webdriver.Firefox()
-        
+        self.driver = webdriver.Firefox()  # TODO: Should be from conf/cmd line
+
     def teardown_method(self, method):
         self.driver.quit()
--- test/tests/page_loading_tests.py
+++ test/tests/page_loading_tests.py
@@ -3,11 +3,11 @@ from test.pages import HomePage, AdminPage
 
 
 class PageLoadingTests(Test):
-    
+
     def test_home_page_loads(self):
-        home = HomePage(self.driver).open().wait_for_loading()    
+        home = HomePage(self.driver).open().wait_for_loading()
         assert home.title == "Home | PARI"
-        
+
     def test_admin_page_loads(self):
-        admin = AdminPage(self.driver).open().wait_for_loading()     
+        admin = AdminPage(self.driver).open().wait_for_loading()
         assert admin.title.startswith("Log in")
