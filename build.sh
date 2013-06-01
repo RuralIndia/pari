7aa31b606496d6cac0cbcf7a36b4f4fbd841b5f2
Jayakumar Chinnappan <chinnappan.jayakumar@gmail.com>
2013-06-01 19:31:37 +0530

    Adding the ability to run functional tests via build.sh, do just 'build.sh functional'.

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
+run_functional_tests() {
+	typeset arg1
+	arg1=$1
+	if [[ "$arg1" == "functional" ]]; then
+		py.test
+		exit $?
+	fi
+}
+
+run_functional_tests $1
+
 flake8 --exclude=migrations --ignore=E501,E225,E128,E126 .
 python manage.py test pari
