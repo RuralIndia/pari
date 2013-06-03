fca079d73d00534f5d5106deefdf5139137ee0b8
manojlds <manojlds@gmail.com>
2013-06-03 10:53:35 +0530

    Adding a management command to import scrapy data

---
+++ pari/article/management/commands/import_scrapy.py
@@ -0,0 +1,54 @@
+from optparse import make_option
+import json
+from datetime import datetime
+
+from django.utils import timezone
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+from mezzanine.generic.models import Keyword, AssignedKeyword
+
+from pari.article.models import Article, Author, Type, Location
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option("-f", "--filename",
+                action="store", type="string", dest="filename"),
+        )
+
+    def handle(self, *args, **options):
+        jsonfilename = options['filename']
+        with open(jsonfilename, 'r') as f:
+            for line in f:
+                jsoncontent = json.loads(line)
+                title = jsoncontent['title'][0].encode('utf-8').strip()
+                print "before"
+                try:
+                    new_article = Article.objects.get(title=title)
+                    print u"Update {0}".format(new_article.title)
+                except ObjectDoesNotExist:
+                    new_article = Article()
+                    new_article.title = title
+                    print u"Import {0}".format(new_article.title)
+
+                new_article.author = Author.objects.get(title=jsoncontent['author'][0])
+                new_article.user = User.objects.get(pk=1)
+                new_article.content = jsoncontent['content']
+                new_article.publish_date = datetime.strptime(jsoncontent['date'], "%B %d, %Y").replace(tzinfo=timezone.utc)
+
+                new_article.save()
+
+                new_article.types.add(Type.objects.get(title='Photo'))
+                new_article.locations.add(Location.objects.get(title='Rural india'))
+
+                for k in jsoncontent['keywords']:
+                    try:
+                        keyword = Keyword.objects.get(title=k)
+                    except:
+                        keyword = Keyword(title=k)
+                        keyword.save()
+
+                    new_article.keywords.add(AssignedKeyword(keyword=keyword))
+
