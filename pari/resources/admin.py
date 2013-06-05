86c63771ba4a55b88f9906c2dc74f332ee66f0fa
manojlds <manojlds@gmail.com>
2013-06-05 16:29:26 +0530

    Adding factoid

--- pari/resources/admin.py
+++ pari/resources/admin.py
@@ -2,16 +2,34 @@ from django.contrib import admin
 
 from mezzanine.core.admin import DisplayableAdmin
 
-from .models import Resource
+from .models import Resource, Factoid
 
+
+class FactoidInline(admin.TabularInline):
+    model = Factoid
+    fk_name = "resource"
+    fields = ('image', 'title')
 
 class ResourceAdmin(DisplayableAdmin):
     fieldsets = (None, {
         "fields": ["title", "description", "gen_description", "embed_source"],
     }),
+    inlines = [
+        FactoidInline,
+    ]
     list_display = ("title", "description")
     list_editable = ()
     list_filter = ()
 
 
+class FactoidAdmin(DisplayableAdmin):
+    fieldsets = (None, {
+        "fields": ["title", "description", "gen_description", "image"],
+    }),
+    list_display = ("admin_thumb", "title", "description")
+    list_editable = ()
+    list_filter = ()
+
+
 admin.site.register(Resource, ResourceAdmin)
+admin.site.register(Factoid, FactoidAdmin)
--- pari/resources/models.py
+++ pari/resources/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 from mezzanine.core.managers import DisplayableManager
 from mezzanine.core.models import Displayable
+from mezzanine.core.fields import FileField
+
+from pari.article.mixins import AdminThumbMixin
 
 
 class Resource(Displayable):
@@ -18,5 +21,19 @@ class Resource(Displayable):
     class Meta:
         verbose_name = _("Resource")
         verbose_name_plural = _("Resources")
+        ordering = ("title",)
+        app_label = "resources"
+
+
+class Factoid(Displayable, AdminThumbMixin):
+    resource = models.ForeignKey("Resource", related_name="factoids", null=True, blank=True)
+    image = FileField(verbose_name=_("Image"),
+                      format="Image", max_length=255, null=True, blank=True)
+
+    admin_thumb_field = "image"
+
+    class Meta:
+        verbose_name = _("Factoid")
+        verbose_name_plural = _("Factoids")
         ordering = ("title",)
         app_label = "resources"
