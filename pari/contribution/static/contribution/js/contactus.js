65c39b123c1ee588c24dc39c98b856bcc5930dbb
Suganthi T <suganthikrishnavathi@gmail.com>
2013-06-11 19:33:52 +0530

    Merge branch 'master' of github.com:RuralIndia/pari
    Conflicts:
    pari/contribution/static/contribution/js/contactus.js
    pari/static/css/contribution_list.less

--- pari/contribution/static/contribution/js/contactus.js
+++ pari/contribution/static/contribution/js/contactus.js
@@ -1,7 +1,5 @@
 $(function () {
     $('.compressed').click(function() {
-        $('.extra').show();
-        $('.captchafield').removeClass('span3')
-        $('.captchafield').addClass('span6')
+        $('.contactform').addClass('expanded');
     });
 });
