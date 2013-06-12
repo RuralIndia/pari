5b307cd036fcd0b9c421d50754b69cf9eb05ffaa
Suganthi T <suganthikrishnavathi@gmail.com>
2013-06-12 15:02:36 +0530

    Added an initial value for the contribution type drop down

--- pari/contribution/fields.py
+++ pari/contribution/fields.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from captcha.fields import ReCaptchaField
 from django.forms import ChoiceField
 from pari.contribution.models import Contribution
@@ -11,6 +12,6 @@ class CaptchaField(ReCaptchaField):
 
 class ContributionsField(ChoiceField):
     def __init__(self, *args, **kwargs):
-        choices = Contribution.objects.values_list('title', 'title')
-        kwargs['choices'] = choices
+        choices = Contribution.objects.values_list('title')
+        kwargs['choices'] = chain([("", "Help us by")], ((choice[0], choice[0]) for choice in choices))
         super(ContributionsField, self).__init__(*args, **kwargs)
