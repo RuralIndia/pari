d651bbe3d7ecbacf9efb8b80150e4d6e05de1cb4
manojlds <manojlds@gmail.com>
2013-06-12 09:59:05 +0530

    Making the hex grid spread across the page

---
+++ pari/resources/static/resources/js/resources.js
@@ -0,0 +1,4 @@
+$(function(){
+    $('.item:nth-child(13n-5)').before($('<div class="item-half"><div class="hexagon hexagon2"><div class="hexagon-in1"><div class="hexagon-in2">'))
+    $('.item:nth-child(13n)').after($('<div class="item-half item-half-right"><div class="hexagon hexagon2"><div class="hexagon-in1"><div class="hexagon-in2">'))
+});
