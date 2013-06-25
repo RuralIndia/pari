ab69b6174ba90fd570e919b123a04ff1242f8e41
manojlds <manojlds@gmail.com>
2013-06-25 12:08:25 +0530

    Adding prod settings

---
+++ pari/settings/prod.py
@@ -0,0 +1,19 @@
+import os
+
+from .base import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pari',                      
+        'USER': 'postgres',
+        'PASSWORD': '!abcd1234',
+        'HOST': 'localhost'
+    }
+}
+
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
+
+COMPRESS_ENABLED = True
+
+DEFAULT_FILE_STORAGE = 'pari.article.storage.ParallelS3Storage'
