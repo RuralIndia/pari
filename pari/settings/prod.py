7aa83087b44b6ff085b3f9e57fff6190a3f0e3f0
manojlds <manojlds@gmail.com>
2013-07-03 10:29:50 +0530

    Adding memcached settings for prod

--- pari/settings/prod.py
+++ pari/settings/prod.py
@@ -19,3 +19,17 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
 COMPRESS_ENABLED = True
 
 DEFAULT_FILE_STORAGE = 'pari.article.storage.ParallelS3Storage'
+
+#Memcached settings
+MIDDLEWARE_CLASSES += (
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
