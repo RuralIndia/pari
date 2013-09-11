3e84b29309fdb465b4b130409ac002272ae075bf
manojlds <manojlds@gmail.com>
2013-09-11 16:46:36 +0530

    Adding a slideshow button to the album images

--- pari/album/static/album/js/lightbox.js
+++ pari/album/static/album/js/lightbox.js
@@ -27,7 +27,9 @@ var Album = {
                 cursor: null,
                 tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 
-                titleSrc: function (item) {
+                titleSrc: $.proxy(function (item) {
+                    var slideshow = this._popup.data('slideshow');
+                    var icon = slideshow ? "pause" : "play";
                     return '<div>'+
                                 '<h4 class="image-heading">'+ item.el.attr('data-photographer') + '</h4>' +
                                 '<p class="image-date">' + item.el.attr('data-date') + '</p>' +
@@ -36,13 +38,14 @@ var Album = {
                                 '<p class="image-caption" data-audio="' + item.el.attr('data-audio') + '">' + item.el.attr('title') + '</p>' +
                                 '<div class="btn-toolbar">'+
                                     '<div class="btn-group">'+
+                                        '<a class="btn btn-slideshow" href="#">Slideshow <i class="icon-' + icon + '"></i></a>'+
                                         '<a class="btn" href="' + item.el.attr('data-url') +'"><i class="icon-share"></i></a>'+
                                         '<a class="btn" href="' + item.el.attr('data-url') +'#comments"><i class="icon-comment-alt"></i></a>'+
                                         '<a class="btn btn-fullscreen" href="#"><i class="icon-fullscreen"></i></a>'+
                                     '</div>'+
                                 '</div>'+
                             '</div>'
-                },
+                }, this),
 
                 markup: '<div class="mfp-figure">'+
                             '<div class="mfp-close"></div>'+
@@ -61,23 +64,8 @@ var Album = {
             closeBtnInside: true,
             callbacks: {
                 updateStatus: $.proxy(function () {
-
                     this._initImage();
-
-                    var audio = $('.mfp-title .image-caption').data('audio');
-                    var controls = $('.mfp-controls');
-                    if(audio && audio != "") {
-                        var slideshow = this._popup.data('slideshow');
-                        slideshow ? this._initPauseButton() : this._initPlayButton();
-
-                        controls.show();
-                        controls.off('click');
-                        controls.on('click', $.proxy(this._toggleWidget, this));
-
-                        this._reloadWidget(audio, slideshow);
-                    } else {
-                        controls.hide();
-                    }
+                    this._initAudio();
                 }, this),
                 close: $.proxy(function () {
                     this._stopWidget();
@@ -107,7 +95,7 @@ var Album = {
 
     _playWidget: function() {
         this._sound.play({
-            onfinish: this._initPlayButton
+            onfinish: $.proxy(this._onSoundFinish, this)
         });
     },
 
@@ -120,10 +108,34 @@ var Album = {
         this._sound.togglePause();
     },
 
+    _updateSlideshowButtonIcon: function () {
+        var slideshow = this._popup.data('slideshow');
+        var slideshowButton = $('.btn-slideshow i');
+        if(slideshow) { 
+            slideshowButton.addClass('icon-pause');
+            slideshowButton.removeClass('icon-play');
+        } else {
+            slideshowButton.addClass('icon-play');
+            slideshowButton.removeClass('icon-pause');
+
+        }
+
+    },
+
     _stopWidget: function() {
         if(this._sound){
             this._sound.stop();
         }
+    },
+
+    _onSoundFinish: function() {
+        var slideshow = this._popup.data('slideshow');
+        if(slideshow) {
+            var magnificPopup = $.magnificPopup.instance;
+            magnificPopup.next();
+            return;
+        }
+        this._initPlayButton();
     },
 
     _initControls: function() {
@@ -139,9 +151,41 @@ var Album = {
             return false;
         });
 
+        $('.btn-slideshow').on('click', $.proxy(function() {
+            var slideshow = this._popup.data("slideshow");
+            if(slideshow) {
+                this._popup.removeData('slideshow');
+            } else {
+                this._popup.data('slideshow', 'true');
+            }
+
+            this._updateSlideshowButtonIcon();
+            this._initAudio();
+
+            return false;
+        }, this));
+
         $('.mfp-figure').on('click', function() {
             $('.mfp-container').removeClass('mfp-container-fullscreen');
         });
+    },
+
+    _initAudio: function() {
+        var audio = $('.mfp-title .image-caption').data('audio');
+        var controls = $('.mfp-controls');
+        var slideshow = this._popup.data('slideshow');
+
+        if(audio && audio != "") {
+            slideshow ? this._initPauseButton() : this._initPlayButton();
+
+            controls.show();
+            controls.off('click');
+            controls.on('click', $.proxy(this._toggleWidget, this));
+
+            this._reloadWidget(audio, slideshow);
+        } else {
+            controls.hide();
+        }
     },
 
     _togglePlayButton: function() {
