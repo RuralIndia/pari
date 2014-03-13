d00f5849190108d1af2b740c3123e62bd4f4352d
manojlds <manojlds@gmail.com>
2014-03-13 12:02:42 +0530

    Showing district along with name

--- pari/faces/urls.py
+++ pari/faces/urls.py
@@ -3,7 +3,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from pari.faces.views import FaceList, FaceDetail
 
 urlpatterns = patterns('pari.faces.views',
-                       url(r'^(?P<alphabet>\w)/', FaceDetail.as_view(template_name="faces/face_detail.html"), name='face-detail'),
+                       url(r'^(?P<alphabet>\w)/', FaceDetail.as_view(template_name="faces/face_detail.html"),
+                           name='face-detail'),
                        url(r'^$', FaceList.as_view(), name='face-list'),
                        )
 
