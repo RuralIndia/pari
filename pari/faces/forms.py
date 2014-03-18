36deddc36407a4f906da9a37485e5f009055d30b
Devarajan <devarajn@thoughtworks.com>
2014-03-18 11:16:02 +0530

    Arvind/Deva - Increased description field max_length for Faces

--- pari/faces/forms.py
+++ pari/faces/forms.py
@@ -1,8 +1,11 @@
+from django import forms
 from pari.article.forms import DisplayableForm, TinyMceWidget
 from pari.faces.models import Face
 
 
 class FaceForm(DisplayableForm):
+    description = forms.CharField(max_length=2000, required=True, widget=forms.Textarea(attrs={'rows': 1, 'cols': 40}))
+
     def __init__(self, *args, **kwargs):
         super(FaceForm, self).__init__(*args, **kwargs)
         self.fields['description'].widget = TinyMceWidget(attrs={'rows': 10, 'cols': 20})
