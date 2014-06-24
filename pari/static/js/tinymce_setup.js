54a156a9dd95c73ca73bbfc0ff828b018cdc7987
Dinesh Kumar <dineshkb@thoughtworks.com>
2014-06-24 17:58:34 +0530

    [Aruna/Dinesh] Adding OpenSans and Merriweather to font-family in tiny-mce editor. partially fixes #32

--- pari/static/js/tinymce_setup.js
+++ pari/static/js/tinymce_setup.js
@@ -1,10 +1,10 @@
 if (typeof tinyMCE != 'undefined') {
     tinymce.init({
         selector: ".mceEditor",
-        remove_script_host : false,
-        convert_urls : false,
+        remove_script_host: false,
+        convert_urls: false,
         relative_urls: false,
-        height : 400,
+        height: 400,
         file_browser_callback: function (field_name, url, type, win) {
             var frame = tinyMCE.activeEditor.windowManager.open({
                 file: window.__filebrowser_url + '?pop=2&type=' + type,
@@ -36,43 +36,43 @@ if (typeof tinyMCE != 'undefined') {
         content_css: "/static/css/tinymce.css",
 
         toolbar1: "insertfile undo redo | styleselect | bold italic lineheight dropcaps blockquote | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image caption",
-        toolbar2: "fullscreen print preview media | fontsizeselect forecolor backcolor",
+        toolbar2: "fullscreen print preview media | fontsizeselect forecolor backcolor fontselect",
         image_advtab: true,
         templates: [
             {title: 'Test template 1', content: 'Test 1'},
             {title: 'Test template 2', content: 'Test 2'}
         ],
-        formats : {
+        formats: {
             dropcaps: {inline: 'span', classes: "dropcaps"},
             lineheight: {block: 'p', styles: {'lineHeight': '%value'}},
             blockquote: {block: 'blockquote', classes: "blockquote"}
         },
 
-        setup: function(ed) {
+        setup: function (ed) {
             ed.addButton('dropcaps', {
                 text: 'Drop Caps',
                 icon: false,
-                onclick: function() {
+                onclick: function () {
                     ed.formatter.apply('dropcaps');
                 }
             });
             ed.addButton('blockquote', {
                 text: 'Blockquote',
                 icon: false,
-                onclick: function() {
+                onclick: function () {
                     ed.formatter.apply('blockquote');
                 }
             });
             ed.addButton('lineheight', {
                 text: 'Line height',
                 icon: false,
-                onclick: function() {
+                onclick: function () {
                     ed.windowManager.open({
                         title: 'Specify line height',
                         body: [
                             {type: 'textbox', name: 'lineheight', label: 'Value'}
                         ],
-                        onsubmit: function(e) {
+                        onsubmit: function (e) {
                             ed.formatter.apply('lineheight', {value: e.data.lineheight});
                         }
                     });
