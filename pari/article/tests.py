88d19ac3362a6a5ad4be9b61db3f5495cffc30af
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2014-07-24 17:18:34 +0530

    Arranging featured image and its related toggle options

--- pari/article/tests.py
+++ pari/article/tests.py
@@ -82,8 +82,8 @@ class ArticleAdminTests(TestCase):
     def test_includes_is_topic_field(self):
         self.assertIn("is_topic", ArticleAdmin.fieldsets[0][1]['fields'])
 
-    def test_includes_pin_to_home_field(self):
-        self.assertIn("pin_to_home", ArticleAdmin.fieldsets[0][1]['fields'])
+    def test_includes_featured_image_and_related_toggle_fields(self):
+        self.assertIn(("featured_image", "allow_featured_image", "pin_to_home", ), ArticleAdmin.fieldsets[0][1]['fields'])
 
     def test_include_pin_to_home_in_list_display(self):
         self.assertIn("pin_to_home", ArticleAdmin.list_display)
