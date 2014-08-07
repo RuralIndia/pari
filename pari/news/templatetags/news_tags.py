173c06b73246b9de6daadbcec85f31d20c8c86a2
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2014-08-07 18:18:07 +0530

    Styling latest articles section

--- pari/news/templatetags/news_tags.py
+++ pari/news/templatetags/news_tags.py
@@ -3,9 +3,8 @@ from mezzanine import template
 register = template.Library()
 
 
-@register.inclusion_tag("news/includes/latest_articles.html", takes_context=True)
-def list_latest_articles(context):
+@register.inclusion_tag("news/includes/articles_gist.html")
+def articles_gist(articles):
     return {
-        'archive_articles': context['new_archive_articles'],
-        'current_articles': context['new_current_articles']
+        'articles': articles,
     }
