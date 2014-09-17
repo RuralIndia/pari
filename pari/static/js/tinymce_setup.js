cb375d12751743561ac48bc11642f5186c3a1a79
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2014-09-17 15:45:54 +0530

    Updating image caption to obey alignment formats & font/size changes. Fixes the issues 95 and 96

--- pari/static/js/tinymce_setup.js
+++ pari/static/js/tinymce_setup.js
@@ -1,4 +1,7 @@
 if (typeof tinyMCE != 'undefined') {
+    var imgContainerClass = 'image-container',
+        imgContainerImgSelector = '.' + imgContainerClass + '>img';
+
     tinymce.init({
         selector: ".mceEditor",
         remove_script_host: false,
@@ -56,7 +59,7 @@ if (typeof tinyMCE != 'undefined') {
             "advlist autolink lists link image charmap print preview hr anchor pagebreak",
             "searchreplace wordcount visualblocks visualchars code fullscreen",
             "insertdatetime media nonbreaking save table contextmenu directionality",
-            "template paste caption"
+            "template paste"
         ],
         content_css: "/static/css/tinymce.css",
 
@@ -69,7 +72,39 @@ if (typeof tinyMCE != 'undefined') {
         ],
         formats: {
             dropcaps: {inline: 'span', classes: "dropcaps"},
-            lineheight: {block: 'p', styles: {'lineHeight': '%value'}}
+            lineheight: {block: 'p', styles: {'lineHeight': '%value'}},
+
+            alignleft: [
+                {selector: 'figure,p,h1,h2,h3,h4,h5,h6,td,th,tr,div,ul,ol,li', styles: {textAlign: 'left'}, defaultBlock: 'div'},
+                {selector: imgContainerImgSelector, collapsed: false, styles: {'display': 'table', 'float': 'left'},
+                    onformat: function(elm, fmt) {
+                        tinymce.activeEditor.dom.setStyles(elm.parentNode, fmt['styles']);
+                    }
+                },
+                {selector: 'img,table', collapsed: false, styles: {'float': 'left'}}
+            ],
+
+            aligncenter: [
+                {selector: 'figure,p,h1,h2,h3,h4,h5,h6,td,th,tr,div,ul,ol,li', styles: {textAlign: 'center'}, defaultBlock: 'div'},
+                {selector: imgContainerImgSelector, collapsed: false, styles: {'display': 'table', float: '', 'margin-left': 'auto', 'margin-right': 'auto'},
+                    onformat: function(elm, fmt) {
+                            tinymce.activeEditor.dom.setStyles(elm.parentNode, fmt['styles']);
+                    }
+                },
+                {selector: 'img', collapsed: false, styles: {display: 'block', marginLeft: 'auto', marginRight: 'auto'}},
+                {selector: 'table', collapsed: false, styles: {marginLeft: 'auto', marginRight: 'auto'}}
+            ],
+
+            alignright: [
+                {selector: 'figure,p,h1,h2,h3,h4,h5,h6,td,th,tr,div,ul,ol,li', styles: {textAlign: 'right'}, defaultBlock: 'div'},
+                {selector: imgContainerImgSelector, collapsed: false, styles: {'display': 'table', 'float': 'right'},
+                    onformat: function(elm, fmt) {
+                        tinymce.activeEditor.dom.setStyles(elm.parentNode, fmt['styles']);
+                    }
+                },
+                {selector: 'img,table', collapsed: false, styles: {'float': 'right'}}
+            ]
+
         },
 
         setup: function (ed) {
@@ -96,6 +131,45 @@ if (typeof tinyMCE != 'undefined') {
                     });
                 }
             });
+
+            ed.addButton('caption', {
+                text: 'Caption',
+                icon: false,
+                onclick: function () {
+                    var element = ed.selection.getNode(),
+                        parent = element.parentElement,
+                        currentCaption = parent.getAttribute('data-label');
+
+                    ed.windowManager.open({
+                        title: currentCaption ? 'Edit Caption' : 'Add Caption',
+                        body: [
+                            {type: 'textbox', name: 'caption', label: 'Caption', value: currentCaption}
+                        ],
+                        onsubmit: function (e) {
+                            var updatedCaption = e.data.caption;
+
+                            if(parent.className === imgContainerClass){
+                                parent.setAttribute('data-label', updatedCaption);
+                                return;
+                            }
+
+                            var dom = ed.dom;
+                            var container = dom.create('a', {'class': imgContainerClass,
+                                'data-label': updatedCaption,
+                                'href': '',
+                                'style': element.getAttribute('style')});
+                            element.setAttribute('style', '');
+                            dom.insertAfter(container, element);
+                            container.appendChild(element);
+
+                            var containerWrapper = dom.create('span', {'class': 'image-container-wrapper' });
+                            dom.insertAfter(containerWrapper, container);
+                            containerWrapper.appendChild(container);
+                        }
+                    });
+                }
+            });
+
         }
     });
 
