7301365d13d5b475abfcf4a2443597d0d0228f29
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2014-12-30 21:27:06 +0530

    Get the settings without reloading the app. Fixes the issue in getting FB/Twitter data. Also change the way cache is happening.

--- pari/sites.py
+++ pari/sites.py
@@ -11,29 +11,31 @@ class PariAdminSite(AdminSite):
         followers = cache.get("twitter_followers")
         if not followers:
             followers = "- NA -"
+            settings.use_editable()
             response = requests.get(
-                settings.SOCIAL_TWITTER,
+                settings.SOCIAL_TWITTER.decode('utf-8'),
                 headers={"accept-language": "en-us, en; q=0.8"})
             if response.ok:
                 content = response.content
                 exists = re.search("(?P<number>\d+) Followers", content)
                 if exists:
                     followers = exists.groups()[0]
-        expires_in_secs = settings.CACHE_MIDDLEWARE_SECONDS
-        cache.set("twitter_followers", followers, expires_in_secs)
+            expires_in_secs = settings.CACHE_MIDDLEWARE_SECONDS
+            cache.set("twitter_followers", followers, expires_in_secs)
         return followers
 
     def facebook_likes(self):
         likes = cache.get("facebook_likes")
         if not likes:
             likes = "- NA -"
+            settings.use_editable()
             if settings.SOCIAL_FACEBOOK_ID:
                 response = requests.get("https://graph.facebook.com/" +
-                                        settings.SOCIAL_FACEBOOK_ID)
+                                        settings.SOCIAL_FACEBOOK_ID.decode('utf-8'))
             if response.ok:
                 likes = response.json()["likes"]
-        expires_in_secs = settings.CACHE_MIDDLEWARE_SECONDS
-        cache.set("facebook_likes", likes, expires_in_secs)
+            expires_in_secs = settings.CACHE_MIDDLEWARE_SECONDS
+            cache.set("facebook_likes", likes, expires_in_secs)
         return likes
 
     def index(self, request, extra_context=None):
